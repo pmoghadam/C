f2cedac72d64572ada0edc20aa369829cca86409
Pejman Moghadam <pmoghadam@yahoo.com>
2024-06-30 00:32:06 +0330

    Update getprotoent-01.c

--- getprotoent-01.c
+++ getprotoent-01.c
@@ -2,6 +2,34 @@
 // Description: read entire /etc/protocols database with getprotoent()
 // Compile: gcc -std=c99 -Wall -Wextra -Werror -pedantic -o getprotoent-01 getprotoent-01.c
 
+/* 
+       man 3 getprotoent
+
+       The  getprotoent() function reads the next entry from the protocols database (see protocols(5))
+       and returns a protoent structure containing the broken-out fields from the entry.  A connection
+       is opened to the database if necessary.
+
+       The protoent structure is defined in <netdb.h> as follows:
+
+           struct protoent {
+               char  *p_name;       // official protocol name
+               char **p_aliases;    // alias list
+               int    p_proto;      // protocol number
+           }
+
+       The members of the protoent structure are:
+
+       p_name The official name of the protocol.
+
+       p_aliases
+              A NULL-terminated list of alternative names for the protocol.
+
+       p_proto
+              The protocol number.
+
+*/
+
+
 #include <stdio.h>
 #include <netdb.h>
 
